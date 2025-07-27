0d9e89ca27164dbe87eac825bab7e1841805dd97
usernoise <usernoise@gmail.com>
2025-07-28 00:40:36 +0300

    фикс

--- testbot.py
+++ testbot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("2222 алеха1!!!")
+    await update.message.reply_text("ИТОГО алеха1!!!")
 
 if __name__ == '__main__':
     TOKEN = "8413828802:AAHIiFNgyuWeSjDEQBTjKlVuwFRQEZTElSA"
