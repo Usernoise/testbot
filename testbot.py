8be6db53e88e4b2c03a4b6b725415988401ecdf3
usernoise <usernoise@gmail.com>
2025-07-27 23:22:55 +0300

    first commit

---
+++ testbot.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("АЛЕ!!!")
+
+if __name__ == '__main__':
+    TOKEN = "8413828802:AAHIiFNgyuWeSjDEQBTjKlVuwFRQEZTElSA"
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Бот запущен...")
+    app.run_polling()
